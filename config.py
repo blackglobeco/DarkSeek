bfea3ffd3d1d94ed3f6aad7a47006769a721d5b2
Black Globe <137195478+blackglobeco@users.noreply.github.com>
2025-02-05 15:09:45 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
 API_KEY = ""
 MODEL = "deepseek/deepseek-r1-distill-llama-70b"
 
-API_ID = ''
-API_HASH = ''
-PHONE = ''
-TWO_STEP_PASS = ''
+BOT_TOKEN = ""
 
 # List of allowed Telegram user IDs
 ALLOWED_USERS = {123456789, 987654321}  # Replace with actual Telegram user IDs
